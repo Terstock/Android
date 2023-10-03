96183977a78f3a13c3aa64169e42932e58170b17
Budonnyi Mykhailo <M.Budonnyy.FIT.122.20@knute.edu.ua>
2023-10-03 14:36:01 +0300

    Added Animal

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -13,6 +13,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.test.ui.theme.TestTheme
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var v3:String
+    private val v1 = "dfdfdf"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.main_activity)
@@ -23,5 +26,28 @@ class MainActivity : ComponentActivity() {
             ///testus2
 
 
+            var v2 = 4.5f
+
+            v2 = 4.5f
+
+            //val v4 = sum(c =5.6, a = 4.5 )
+            val v5 = myOuterFun(4, ::myInnerFun)
+            val v6 = myOuterFun(3, {
+                //dfdfd
+                it
+                return@myOuterFun "fgfgf"
+            })
     }
+
+    fun myOuterFun(number:Int, innerFun:(myString: String) -> String):Int{
+        //something
+        val call = innerFun("Some string")
+        return number + number
+    }
+
+    fun myInnerFun(myString: String): String{
+        //dfdfd
+        return "sdsdsd"
+    }
+    //fun sum(a:Double, b:Double = 0.0, c:Double=-1.0): Double{}
 }
